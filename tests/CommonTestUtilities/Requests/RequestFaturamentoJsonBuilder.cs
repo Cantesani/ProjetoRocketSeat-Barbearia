5d84ed923ce3d116005b32bf680a447e6b20d2e7
cantesani <cantesani@hotmail.com>
2025-05-24 09:13:29 -0300

    finalizacao projeto

---
+++ tests/CommonTestUtilities/Requests/RequestFaturamentoJsonBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Barbearia.Communication.Enums;
+using Barbearia.Communication.Request;
+using Bogus;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestFaturamentoJsonBuilder
+    {
+        public static RequestFaturamentoJson Build()
+        {
+            return new Faker<RequestFaturamentoJson>()
+                   .RuleFor(x => x.Titulo, faker => faker.Commerce.ProductName())
+                   .RuleFor(x => x.Descricao, faker => faker.Commerce.ProductDescription())
+                   .RuleFor(x => x.Data, faker => faker.Date.Past())
+                   .RuleFor(x => x.TipoPagto, faker => faker.PickRandom<TipoPagto>())
+                   .RuleFor(x => x.Valor, faker => faker.Random.Decimal(min: 1, max: 1000));
+        }
+    }
+}
--- tests/Validators.Tests/Faturamento/Create/FaturamentoValidationTests.cs
+++ tests/Validators.Tests/Faturamento/Create/FaturamentoValidationTests.cs
@@ -1,5 +1,9 @@
 ﻿using Barbearia.Application.UseCases;
+using Barbearia.Communication.Enums;
 using Barbearia.Communication.Request;
+using Barbearia.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
 
 namespace Validators.Tests.Faturamento.Create
 {
@@ -10,21 +14,72 @@ namespace Validators.Tests.Faturamento.Create
         {
             //Arrange
             var validator = new FaturamentoValidation();
-            var request = new RequestFaturamentoJson
-            {
-                Valor = 100,
-                Data = DateTime.Now,
-                Descricao = "Descrição teste",
-                Titulo = "Celular",
-                TipoPagto = Barbearia.Communication.Enums.TipoPagto.Debito
-            };
-
-
+            var request = RequestFaturamentoJsonBuilder.Build();
 
             //Act
+            var result = validator.Validate(request);
 
             //Assert
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Error_Title_Empty(string titulo)
+        {
+            //Arrange
+            var validator = new FaturamentoValidation();
+            var request = RequestFaturamentoJsonBuilder.Build();
+            request.Titulo = titulo;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(x => x.ErrorMessage.Equals(ResourceErrorMessages.TITULO_OBRIGATORIO));
 
         }
+
+        [Fact]
+        public void Error_Data_Futuro()
+        {
+            //Arrange
+            var validator = new FaturamentoValidation();
+            var request = RequestFaturamentoJsonBuilder.Build();
+            request.Data = DateTime.UtcNow.AddDays(1);
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(x => x.ErrorMessage.Equals(ResourceErrorMessages.DATA_PRECISA_SER_ANTERIOR_A_ATUAL));
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-7)]
+        public void Error_Valor_Invalido(decimal valor)
+        {
+            //Arrange
+            var validator = new FaturamentoValidation();
+            var request = RequestFaturamentoJsonBuilder.Build();
+            request.Valor = valor;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(x => x.ErrorMessage.Equals(ResourceErrorMessages.VALOR_PRECISA_SER_MAIOR_QUE_ZERO));
+
+        }
+
+
     }
 }
